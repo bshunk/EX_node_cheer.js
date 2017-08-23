2f65ff145763ae0557182e0f9ea8a5e9305ee933
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-23 15:27:58 -0500

    get letters to print out requirements, but now work on getting to display a little more dynamically

---
+++ cheer.js
@@ -0,0 +1,24 @@
+let cheerLetters = ['J!', 'O!', 'H!', 'N!', 'D!', 'O!', 'E!'];
+
+console.log('Give me a', cheerLetters[0]);
+console.log('Give me an', cheerLetters[1]);
+console.log('Give me a', cheerLetters[2]);
+console.log('Give me a', cheerLetters[3]);
+console.log('Give me a', cheerLetters[4]);
+console.log('Give me an', cheerLetters[5]);
+console.log('Give me an', cheerLetters[6]);
+console.log('What does that spell?');
+console.log('JOHN DOE!');
+console.log('Note: the "a" vs. "an"');
+
+
+// $ node cheer.js
+// Give me a J!
+// Give me an O!
+// Give me an H!
+// Give me an N!
+// Give me a D!
+// Give me an O!
+// Give me an E!
+// What does that spell?
+// JOHN DOE!
